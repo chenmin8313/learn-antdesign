2073c99a64983bc9b34eca841ec55a3d161bb207
chenmin8313 <156200576+chenmin8313@users.noreply.github.com>
2024-01-10 21:39:10 +0800

    修改title

--- config/config.ts
+++ config/config.ts
@@ -76,7 +76,7 @@ export default defineConfig({
    * @name layout 插件
    * @doc https://umijs.org/docs/max/layout-menu
    */
-  title: 'Ant Design Pro',
+  title: '学习 Antd Design',
   layout: {
     locale: true,
     ...defaultSettings,
